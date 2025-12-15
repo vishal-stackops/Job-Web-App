cc3d8e308fdc9f6c5f55c2523befa82873b61f6c
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-15 19:14:48 +0530

    Update axios import paths in RecruiterProfile.js

--- src/components/recruiter/RecruiterProfile.js
+++ src/components/recruiter/RecruiterProfile.js
@@ -3,8 +3,8 @@ import RecruiterNavbar from './RecruiterNavbar';
 import './RecruiterProfile.css';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
-import axios from "../config/axios";
-import API_BASE_URL from "../config/axios";
+import axios from "../../config/axios";
+import API_BASE_URL from "../../config/axios";
 function RecruiterProfile() {
   const { recruiterId: paramRecruiterId } = useParams();
   const navigate = useNavigate();
