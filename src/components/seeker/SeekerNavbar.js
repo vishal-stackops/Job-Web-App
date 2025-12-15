3420e12f2a70db54abf2d5594dc8e14d4b904f00
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-15 19:42:03 +0530

    Update SeekerNavbar.js

--- src/components/seeker/SeekerNavbar.js
+++ src/components/seeker/SeekerNavbar.js
@@ -60,7 +60,7 @@ function SeekerNavbar({ onSearch }) {
                 onChange={handleSearchInputChange}
               />
               <button type="submit" className="search-icon-btn" title="Search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                <svg xmlns="www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.099zm-5.242 1.656a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
                 </svg>
               </button>
