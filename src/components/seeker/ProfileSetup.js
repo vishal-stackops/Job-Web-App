a8ae512a71e083c4ae0bc1527b9b3d23e1d6affa
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-15 19:24:18 +0530

    Refactor axios import in ProfileSetup.js

--- src/components/seeker/ProfileSetup.js
+++ src/components/seeker/ProfileSetup.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import './ProfileSetup.css';
 import axios from "../../config/axios";
 import API_BASE_URL from "../../config/axios";
