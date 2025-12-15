8431099ea35187e071e1577445464112b3f775d0
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-15 19:21:36 +0530

    Refactor axios import in Applications.js

--- src/components/recruiter/Applications.js
+++ src/components/recruiter/Applications.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import RecruiterNavbar from './RecruiterNavbar';
-import axios from 'axios';
+// import axios from 'axios';
 import './Applications.css';
 import axios from "../../config/axios";
 import API_BASE_URL from "../../config/axios";
