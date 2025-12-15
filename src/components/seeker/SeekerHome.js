0ee73afc733aee446ee9713638632214450e38b8
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-15 19:12:35 +0530

    Fix import paths for axios and API_BASE_URL

--- src/components/seeker/SeekerHome.js
+++ src/components/seeker/SeekerHome.js
@@ -5,8 +5,8 @@ import ThemeToggle from '../ThemeToggle';
 import './SeekerHome.css';
 import JobCard from './JobCard';
 import { useProfileCheck } from '../../hooks/useProfileCheck';
-import axios from "../config/axios";
-import API_BASE_URL from "../config/axios";
+import axios from "../../config/axios";
+import API_BASE_URL from "../../config/axios";
 
 function SeekerHome() {
   const [jobs, setJobs] = useState([]);
