1fff632876d4c7c9bab52ac772ee34c95d2cfc10
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-16 11:42:34 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
 import './Signup.css';
-import axios from "../config/axios";
+import API_BASE_URL from "../config/axios";
 
 
 function Signup() {
