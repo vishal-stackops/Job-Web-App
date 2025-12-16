dbb546fbca6a260f61a9b842471bbd65d98233b7
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-16 11:50:59 +0530

    Update useProfileCheck.js

--- src/hooks/useProfileCheck.js
+++ src/hooks/useProfileCheck.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from "../config/axios";
+import axios from "axios";
+import API_BASE_URL from "../config/api";
 
 export const useProfileCheck = () => {
   const [isChecking, setIsChecking] = useState(true);
