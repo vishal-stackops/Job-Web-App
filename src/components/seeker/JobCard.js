ca35e95b4e0b0c83cf4fb545c017bff500f6f8d8
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-16 11:53:40 +0530

    Update JobCard.js

--- src/components/seeker/JobCard.js
+++ src/components/seeker/JobCard.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../../App';
 import './JobCard.css';
-import axios from "../../config/axios";
+import axios from "axios";
+import API_BASE_URL from "../config/api";
 
 
 function JobCard({ job }) {
