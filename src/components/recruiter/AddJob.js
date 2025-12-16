d8feec59bee003b612374cd66c5def7c31e087c9
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-16 11:51:35 +0530

    Update AddJob.js

--- src/components/recruiter/AddJob.js
+++ src/components/recruiter/AddJob.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RecruiterNavbar from './RecruiterNavbar';
 import './AddJob.css';
-import API_BASE_URL from "../../config/axios";
+import axios from "axios";
+import API_BASE_URL from "../config/api";
 
 
 function AddJob() {
