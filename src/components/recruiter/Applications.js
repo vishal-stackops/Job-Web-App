a0cc4f407c96f9ea333844e68772e9810a068634
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-16 12:07:15 +0530

    Update Applications.js

--- src/components/recruiter/Applications.js
+++ src/components/recruiter/Applications.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import RecruiterNavbar from './RecruiterNavbar';
 import './Applications.css';
 import axios from "axios";
-import API_BASE_URL from "../config/api";
+import API_BASE_URL from "../../config/api";
 
 function Applications() {
   const [applications, setApplications] = useState([]);
