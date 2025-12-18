b6d3fe38472652e2d40857add900308d3af7cb2c
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-18 10:30:45 +0530

    Update RecruiterProfileUpdate.js

--- src/components/recruiter/RecruiterProfileUpdate.js
+++ src/components/recruiter/RecruiterProfileUpdate.js
@@ -37,7 +37,7 @@ function RecruiterProfileUpdate() {
     setLoading(true);
     setError('');
     try {
-      const res = await axios.get(`/api/recruiters/${recruiterId}/profile`, { withCredentials: true });
+      const res = await axios.get(`${API_BASE_URL}/api/recruiters/${recruiterId}/profile`, { withCredentials: true });
       setProfile(res.data);
     } catch {
       setError('Profile not found.');
