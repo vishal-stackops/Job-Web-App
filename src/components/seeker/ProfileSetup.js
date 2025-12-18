803704823f84a14869f40db1cd757999fb412c6b
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2025-12-18 13:22:09 +0530

    Update ProfileSetup.js

--- src/components/seeker/ProfileSetup.js
+++ src/components/seeker/ProfileSetup.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ProfileSetup.css';
 import axios from "axios";
+import SeekerNavbar from './SeekerNavbar';
 import API_BASE_URL from "../../config/api";
 
 function ProfileSetup() {
