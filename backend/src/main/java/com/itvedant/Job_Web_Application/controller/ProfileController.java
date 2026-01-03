c0d781e5c1195268d1b75662cbd411ce83fa49c6
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2026-01-03 11:56:28 +0530

    Update ProfileController.java

--- backend/src/main/java/com/itvedant/Job_Web_Application/controller/ProfileController.java
+++ backend/src/main/java/com/itvedant/Job_Web_Application/controller/ProfileController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/profiles")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+
 public class ProfileController {
     
     @Autowired
