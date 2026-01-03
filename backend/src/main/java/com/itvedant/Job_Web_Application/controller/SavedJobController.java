1165688f80de91812ffa681c93bcb1cbf5b6adcc
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2026-01-03 11:58:33 +0530

    Update SavedJobController.java

--- backend/src/main/java/com/itvedant/Job_Web_Application/controller/SavedJobController.java
+++ backend/src/main/java/com/itvedant/Job_Web_Application/controller/SavedJobController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/saved-jobs")
-@CrossOrigin(origins = "http://localhost:3000")
+
 public class SavedJobController {
     
     @Autowired
