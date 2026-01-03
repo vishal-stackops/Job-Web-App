b6894c29ea970030d5311c6b0889f30ab7b6e1d9
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2026-01-03 11:57:53 +0530

    Update ApplicationController.java

--- backend/src/main/java/com/itvedant/Job_Web_Application/controller/ApplicationController.java
+++ backend/src/main/java/com/itvedant/Job_Web_Application/controller/ApplicationController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/applications")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+
 public class ApplicationController {
 
     @Autowired
