37afbf2bd930b284632105f9990a2609a909d7ea
Vishal Rajput <158999158+vishal-stackops@users.noreply.github.com>
2026-01-30 10:47:49 +0530

    Comment out API_BASE_URL import in useProfileCheck

--- src/hooks/useProfileCheck.js
+++ src/hooks/useProfileCheck.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
-import API_BASE_URL from "../config/api";
+//import API_BASE_URL from "../config/api";
 
 export const useProfileCheck = () => {
   const [isChecking, setIsChecking] = useState(true);
